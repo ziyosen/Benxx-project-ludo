38e1facfc589c32fa39a17328cebc5f3d2291607
Strange-Quark-007 <masteryasin@ymail.com>
2024-01-01 14:52:04 +0530

    board layout component

---
+++ src/Components/BoardLayout.tsx
@@ -0,0 +1,23 @@
+'use client';
+import PlayerCard from "./PlayerCard";
+import DynamicLudoBoard from "./DynamicLudoBoard";
+
+const BoardLayout = () => {
+  return (
+    <div className="">
+      <div className="my-4 flex justify-between">
+        <PlayerCard key="P2" id="P2" />
+        <PlayerCard key="P3" id="P3" />
+      </div>
+      <div className="w-auto h-auto">
+        <DynamicLudoBoard />
+      </div>
+      <div className="my-4 flex justify-between">
+        <PlayerCard key="P1" id="P1" />
+        <PlayerCard key="P4" id="P4" />
+      </div>
+    </div>
+  );
+};
+
+export default BoardLayout;
