23d190fa3ddfd40d18da616c4f343b05ed18ba19
Strange-Quark-007 <masteryasin@ymail.com>
2024-01-01 14:51:20 +0530

    play page

---
+++ src/app/play/page.tsx
@@ -0,0 +1,16 @@
+'use client';
+import BoardLayout from '@/Components/BoardLayout';
+import { store } from '@/redux/store';
+import { Provider } from "react-redux";
+
+const Game = () => {
+  return (
+    <Provider store={store}>
+      <main className="flex justify-center items-center sm:scale-100 scale-50 w-full h-full">
+        <BoardLayout />
+      </main>
+    </Provider >
+  );
+};
+
+export default Game;
